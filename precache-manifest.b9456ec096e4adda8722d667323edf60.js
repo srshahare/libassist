d5655db4124aef658903328639564f76c7a49b17
Shashank  Shahare <srshahare22@gmail.com>
2020-04-07 22:46:11 -0700

    Updates

---
+++ precache-manifest.b9456ec096e4adda8722d667323edf60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66104b26bc2bd3ba044846b3e6673cd7",
+    "url": "/libassist/index.html"
+  },
+  {
+    "revision": "ec45fd9aa233ac711358",
+    "url": "/libassist/static/css/main.c641cd35.chunk.css"
+  },
+  {
+    "revision": "73026441e8ca2016ea6f",
+    "url": "/libassist/static/js/2.34f68e1c.chunk.js"
+  },
+  {
+    "revision": "b46834a9d3d0db4257d134d0cba08264",
+    "url": "/libassist/static/js/2.34f68e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec45fd9aa233ac711358",
+    "url": "/libassist/static/js/main.46a8f702.chunk.js"
+  },
+  {
+    "revision": "9ad09539d4dafbfea441",
+    "url": "/libassist/static/js/runtime-main.26e71355.js"
+  }
+]);
